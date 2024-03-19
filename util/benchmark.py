207917f5693490d08f1cfbd48612e911eb73058a
Hyo <hyogrin@gmail.com>
2024-03-19 04:15:09 +0000

    add perf benchmark

--- util/benchmark.py
+++ util/benchmark.py
@@ -6,17 +6,23 @@ import csv
 
 logger = logging.getLogger(__name__)
 
-def benchmark_latency(model, input_data, data_length, num_infers=10):
+def benchmark_latency(model, input_data, data_length=10, num_infers=10, compiled=True):
     logger.info(f"Measuring latency for sequence length={data_length}, num_infers={num_infers}")
     payload = generate_sample_inputs(input_data, data_length)
     latencies = []
     # warm up
     for _ in range(5):
-        _ = model(payload)
+        if(compiled):
+            _ = model(payload)
+        else:
+            _ = model.predict(payload)
     # Timed run
     for _ in range(num_infers):
         start_time = perf_counter()
-        _ =  model(payload)
+        if(compiled):
+            _ = model(payload)
+        else:
+            _ = model.predict(payload)
         latency = perf_counter() - start_time
         latencies.append(latency)
     # Compute run statistics
@@ -25,6 +31,9 @@ def benchmark_latency(model, input_data, data_length, num_infers=10):
     time_p95_ms = 1000 * np.percentile(latencies,95)
     time_p99_ms = 1000 * np.percentile(latencies,99)    
     return {"avg_ms": time_avg_ms, "std_ms": time_std_ms, "p95_ms": time_p95_ms, "p99_ms": time_p95_ms}
+
+
+
  
 def generate_sample_inputs(input_data, data_length=10):
     input_data = input_data[:data_length]
